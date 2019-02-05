9c30c7f8fc3a15399247f3321130d861a14baa50
Deliman206 <smiller206@me.com>
2019-02-04 16:44:18 -0800

    called api and showed results in View

---
+++ GitHired-MVC/GitHired-MVC/Models/JobPosting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHired_MVC.Models
+{
+    public class JobPosting
+    {
+        public Job job { get; set; }
+        public List<string> skills { get; set; }
+    }
+}
