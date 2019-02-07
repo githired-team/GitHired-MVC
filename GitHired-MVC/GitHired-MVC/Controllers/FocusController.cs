f88820f64f0dce4d5d46701b2e685e87b65e6e20
juliely8619 <july8619@yahoo.com>
2019-02-06 22:51:28 -0800

    have focus data if user loged in before. but wonky thing with creat from index pge then

--- GitHired-MVC/GitHired-MVC/Controllers/FocusController.cs
+++ GitHired-MVC/GitHired-MVC/Controllers/FocusController.cs
@@ -29,9 +29,16 @@ namespace GitHired_MVC.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index(Focus specificUserObj)
+        //public async Task<IActionResult> Index(Focus specificUserObj)
+        //{
+        //    int specificUserID = specificUserObj.UserID;
+        //    //var returnInfo = _context.Focus.Include(fu => fu.UserID);
+        //    return View(await _focus.GetFocus(specificUserID));
+        //    //return View();
+        //}
+        public async Task<IActionResult> Index(Focus focus)
         {
-            int specificUserID = specificUserObj.UserID;
+            int specificUserID = focus.UserID;
             //var returnInfo = _context.Focus.Include(fu => fu.UserID);
             return View(await _focus.GetFocus(specificUserID));
             //return View();
@@ -42,7 +49,10 @@ namespace GitHired_MVC.Controllers
         {
             //var returnInfo = _context.Focus.Include(fu => fu.UserID);
             var focuses = await _focus.GetFocus(id);
-            return RedirectToAction("Index", focuses);
+            var focObj = from o in focuses
+                         .Where(i => i.UserID == id)
+                         select o;
+            return RedirectToAction("Index", focObj.FirstOrDefault());
             //return View();
         }
 
@@ -59,10 +69,6 @@ namespace GitHired_MVC.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([Bind("UserID, Name, DesiredPosition, Location, Skill, ResumeLink, CoverLetter")] Focus focus)
         {
-
-
-            
-
             if (ModelState.IsValid)
             {
                 await _focus.CreateFocus(focus);
